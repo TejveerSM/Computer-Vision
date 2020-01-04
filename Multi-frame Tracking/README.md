bb418485973fda1b3aae95970c88fb60cc0ce4b9
TejveerM <tejveer.m@gmail.com>
2020-01-03 22:42:26 -0600

    Multi-frame tracking (changes)

--- Multi-frame Tracking/README.md
+++ Multi-frame Tracking/README.md
@@ -1,1 +1,7 @@
 # Multi-frame Tracking
+
+Tracking a template in multiple frames using Inverse Compositional Image Alignment algorithm
+	- feature matching based on SIFT descriptors
+	- affine transformation by applying RANSAC method on feature correspondences
+	- image warping
+	- inverse compositional image alignment
--- Multi-frame Tracking/Run.m
+++ Multi-frame Tracking/Run.m
@@ -1,24 +1,24 @@
-I1 = imread('I1.jpeg');
+I1 = imread('template.jpeg');
 if (ndims(I1)>2)
     I1 = rgb2gray(I1);
 end
 
-I2 = imread('I2.jpeg');
+I2 = imread('frame1.jpeg');
 if (ndims(I2)>2)
     I2 = rgb2gray(I2);
 end
 
-I3 = imread('I3.jpeg');
+I3 = imread('frame2.jpeg');
 if (ndims(I3)>2)
     I3 = rgb2gray(I3);
 end
 
-I4 = imread('I4.jpeg');
+I4 = imread('frame3.jpeg');
 if (ndims(I4)>2)
     I4 = rgb2gray(I4);
 end
 
-I5 = imread('I5.jpeg');
+I5 = imread('frame4.jpeg');
 if (ndims(I5)>2)
     I5 = rgb2gray(I5);
 end
